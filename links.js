724b209fd8e4910e17c89e1b876988b7ae868437
Ritesh Vishwakarma <rtr.riteshvish@gmail.com>
2017-12-13 16:41:56 +0530

    best way to detect a mobile device

--- links.js
+++ links.js
@@ -290,3 +290,6 @@ body {
     -webkit-user-drag: none;
 }
 ----------------------------
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ // some code..
+}
