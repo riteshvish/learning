636336d4ceccceba7937c7337fc4be022d8d858e
Ritesh Vishwakarma <rtr.riteshvish@gmail.com>
2018-09-17 11:17:03 +0530

    isValidDate
    isValidDate

--- links.js
+++ links.js
@@ -1,5 +1,14 @@
 
 =========================================================================================================================================
+https://stackoverflow.com/questions/18758772/how-do-i-validate-a-date-in-this-format-yyyy-mm-dd-using-jquery/18758944
+function isValidDate(dateString) {
+  var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if(!dateString.match(regEx)) return false;  // Invalid format
+  var d = new Date(dateString);
+  if(Number.isNaN(d.getTime())) return false; // Invalid date
+  return d.toISOString().slice(0,10) === dateString;
+}
+=========================================================================================================================================
 Angular2-6 form validaion on button click/blur
 	import { BrowserModule } from '@angular/platform-browser';
 import { NgModule }  from '@angular/core';
